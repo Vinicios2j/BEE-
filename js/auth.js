2fbed93b48da647b8cfdf45f5efddbccc4117011
Jose Vinicios Batisata dos Santos <jose.vinicius.batista292@gmail.com>
2026-03-31 14:11:28 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -5,28 +5,24 @@ document.querySelector('.login-form').addEventListener('submit', async (e) => {
     const senha = document.getElementById('password').value;
 
     try {
-        const response = await fetch('https://apibee.squareweb.app/api/login-site', {
+        const response = await fetch(`${API_URL}/api/login-site`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            credentials: 'include', // 🔥 ESSENCIAL
+            credentials: 'include',
             body: JSON.stringify({ nome, senha })
         });
 
         const data = await response.json();
 
         if (response.ok) {
-            // 🔥 REDIRECIONA PRO DASHBOARD
             window.location.href = "../pages/perfil.html";
-
         } else {
             alert("❌ " + data.message);
         }
-
     } catch (error) {
         console.error(error);
         alert("Erro ao conectar com o servidor");
     }
 });
-
